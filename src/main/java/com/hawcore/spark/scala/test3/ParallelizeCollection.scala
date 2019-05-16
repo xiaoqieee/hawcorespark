9554d62d27dfa33b255d090967c65e8d97d43a68
xiaoqieee <fengqi3389@sina.com>
2019-05-16 12:58:10 +0800

    项目结构调整

--- src/main/java/com/hawcore/spark/scala/test3/ParallelizeCollection.scala
+++ src/main/java/com/hawcore/spark/scala/test3/ParallelizeCollection.scala
@@ -13,7 +13,7 @@ object ParallelizeCollection {
 
     val sc = new SparkContext(conf)
 
-    val numbers = Array(1, 2, 3, 4, 5, 6, 7, 89, 10)
+    val numbers = Array(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
 
     val numberRDD = sc.parallelize(numbers, 5)
 
