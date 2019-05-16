aac15b7166b288ac13c22410d6fd55d17dedfb6d
xiaoqieee <fengqi3389@sina.com>
2019-05-16 20:26:48 +0800

    transformation

--- src/main/java/com/hawcore/spark/java/test1/TransformationOperation.java
+++ src/main/java/com/hawcore/spark/java/test1/TransformationOperation.java
@@ -4,10 +4,12 @@ import org.apache.spark.SparkConf;
 import org.apache.spark.SparkContext;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.function.FlatMapFunction;
 import org.apache.spark.api.java.function.Function;
 import org.apache.spark.api.java.function.VoidFunction;
 
 import java.util.Arrays;
+import java.util.Iterator;
 import java.util.List;
 
 /**
@@ -16,28 +18,78 @@ import java.util.List;
  */
 public class TransformationOperation {
     public static void main(String[] args) {
-        map();
-    }
-
-    private static void map() {
         SparkConf conf = new SparkConf().setAppName("TransformationOperation").setMaster("local");
         JavaSparkContext sc = new JavaSparkContext(conf);
 
-        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5);
-        JavaRDD<Integer> numberRDD = sc.parallelize(numbers);
-        JavaRDD<Integer> multipleNumberRDD = numberRDD.map(new Function<Integer, Integer>() {
-            @Override
-            public Integer call(Integer integer) throws Exception {
-                return integer * 2;
-            }
-        });
-        multipleNumberRDD.foreach(new VoidFunction<Integer>() {
-            @Override
-            public void call(Integer integer) throws Exception {
-                System.out.println(integer);
-            }
-        });
+//        map(sc);
+
+//        filter(sc);
+
+        flatMap(sc);
 
         sc.close();
+    }
+
+    private static void map(JavaSparkContext sc) {
+
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5);
+        JavaRDD<Integer> numberRDD = sc.parallelize(numbers);
+
+//        JavaRDD<Integer> multipleNumberRDD = numberRDD.map(new Function<Integer, Integer>() {
+//            @Override
+//            public Integer call(Integer integer) throws Exception {
+//                return integer * 2;
+//            }
+//        });
+//        multipleNumberRDD.foreach(new VoidFunction<Integer>() {
+//            @Override
+//            public void call(Integer integer) throws Exception {
+//                System.out.println(integer);
+//            }
+//        });
+
+        numberRDD.map(v -> v * 2).foreach(s -> System.out.println(s));
+    }
+
+    private static void filter(JavaSparkContext sc) {
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+        JavaRDD<Integer> numberRDD = sc.parallelize(numbers, 5);
+
+//        JavaRDD<Integer> evenNumberRDD = numberRDD.filter(new Function<Integer, Boolean>() {
+//            @Override
+//            public Boolean call(Integer integer) throws Exception {
+//                return integer % 2 == 0;
+//            }
+//        });
+//        evenNumberRDD.foreach(new VoidFunction<Integer>() {
+//            @Override
+//            public void call(Integer integer) throws Exception {
+//                System.out.println(integer);
+//            }
+//        });
+
+        numberRDD.filter(v -> v % 2 == 0).foreach(v -> System.out.println(v));
+    }
+
+    private static void flatMap(JavaSparkContext sc) {
+
+        List<String> lineList = Arrays.asList("hello you", "hello me", "hello world");
+        JavaRDD<String> lines = sc.parallelize(lineList);
+
+//        JavaRDD<String> words = lines.flatMap(new FlatMapFunction<String, String>() {
+//            @Override
+//            public Iterator<String> call(String s) throws Exception {
+//                return Arrays.asList(s.split(" ")).iterator();
+//            }
+//        });
+//
+//        words.foreach(new VoidFunction<String>() {
+//            @Override
+//            public void call(String s) throws Exception {
+//                System.out.println(s);
+//            }
+//        });
+
+        lines.flatMap(s -> Arrays.asList(s.split(" ")).iterator()).foreach(s -> System.out.println(s));
     }
 }
---
+++ src/main/java/com/hawcore/spark/scala/test3/TransformationOperation.scala
@@ -0,0 +1,46 @@
+package com.hawcore.spark.scala.test3
+
+import java.util
+import java.util.{Arrays, List}
+
+import org.apache.spark.{SparkConf, SparkContext}
+
+/**
+  * @author xn025665   
+  * @date Create on 2019/5/16 19:59 
+  */
+object TransformationOperation {
+
+  def main(args: Array[String]): Unit = {
+    val conf = new SparkConf().setAppName("TransformationOperation").setMaster("local")
+
+    val sc = new SparkContext(conf)
+
+    //    map(sc)
+
+//    filter(sc)
+
+      flatMap(sc)
+  }
+
+  def map(sc: SparkContext): Unit = {
+
+    val numbers = Array(1, 2, 3, 4, 5)
+
+    sc.parallelize(numbers).map(v => v * 2).foreach(println(_))
+  }
+
+  def filter(sc: SparkContext): Unit = {
+
+    val numbers = Array(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+
+    sc.parallelize(numbers, 2).filter(v => v % 2 == 0).foreach(println(_))
+  }
+
+  def flatMap(sc: SparkContext): Unit = {
+    val lineList = Array("hello you", "hello me", "hello world")
+    sc.parallelize(lineList).flatMap(s => s.split(" ")).foreach(println(_))
+
+  }
+
+}
