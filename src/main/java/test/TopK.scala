a1041d61ffa69b055ee070b2abd5f7beaea32f81
xiaoqieee <fengqi3389@sina.com>
2019-05-16 11:39:19 +0800

    调整格式

--- src/main/java/test/TopK.scala
+++ src/main/java/test/TopK.scala
@@ -6,11 +6,15 @@ object TopK {
 
   def main(args: Array[String]): Unit = {
     System.setProperty("hadoop.home.dir", "D:\\base\\hadoop-common-2.6.0-bin")
-    val spark = new SparkContext("local","TopK")
+    val spark = new SparkContext("local", "TopK")
     val inputFile = "D:\\data\\spark\\tempdata\\spark.txt"
-    val count = spark.textFile(inputFile).flatMap(line => line.split(" ")).map(word => (word,1)).reduceByKey((a,b)=>{
-      a+b
-    }).collect();
+    val count = spark.textFile(inputFile)
+      .flatMap(line => line.split(" "))
+      .map(word => (word, 1))
+      .reduceByKey((a, b) => {
+        a + b
+      })
+      .collect();
     count.foreach(print)
   }
 }
